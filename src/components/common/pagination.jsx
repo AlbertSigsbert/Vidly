22d067b1ecb5ea1092495f110af30d864f797790
Albert <ishengomaalbert@gmail.com>
2021-11-05 08:29:03 +0300

    Finalize pagination component & perform typechecking for it

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import _ from "lodash";
+import PropTypes from "prop-types";
 
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange} = props;
   const pageCount = Math.ceil(itemsCount / pageSize);
   if (pageCount === 1) return null;
   const pages = _.range(1, pageCount + 1);
@@ -11,8 +12,8 @@ const Pagination = (props) => {
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a className="page-link">{page}</a>
+          <li key={page} className={page === currentPage ? 'page-item active': 'page-item'}>
+            <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
           </li>
         ))}
       </ul>
@@ -20,4 +21,10 @@ const Pagination = (props) => {
   );
 };
 
+Pagination.propTypes = {
+   itemsCount:PropTypes.number.isRequired,
+   pageSize:PropTypes.number.isRequired,
+   currentPage:PropTypes.number.isRequired,
+   onPageChange:PropTypes.func.isRequired,
+};
 export default Pagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,12 +1,15 @@
-import React  from "react";
+import React from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
+
+
 class Movies extends React.Component {
   state = {
     movies: getMovies(),
-    pageSize: 5
-   
+    currentPage: 1,
+    pageSize: 4,
   };
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -14,26 +17,27 @@ class Movies extends React.Component {
   };
 
   handleLike = (movie) => {
-      const movies = [...this.state.movies];
-      const index = movies.indexOf(movie);
-      movies[index] = {...movies[index]};
-      movies[index].liked = !movies[index].liked;
-      this.setState({ movies })
-  }
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
 
-  handlePageChange = (e) => {
-    console.log('Pagination clicked');
-  
-    e.preventDefault()
-  }
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies:allMovies } = this.state;
     if (count === 0)
       return (
         <p className=" container font-weight-bold lead py-4">
           There are no movies
         </p>
       );
+     
+    const movies = paginate(allMovies,currentPage, pageSize);
 
     return (
       <main className="container">
@@ -47,18 +51,23 @@ class Movies extends React.Component {
               <th scope="col">Genre</th>
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
-             <th/>
-             <th/>
+              <th />
+              <th />
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id} id={movie._id}>
                 <th scope="row">{movie.title}</th>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
-                <td><Like liked={movie.liked}  onClick={ () => this.handleLike(movie) } /></td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     className="btn btn-danger btn-sm"
@@ -71,7 +80,12 @@ class Movies extends React.Component {
             ))}
           </tbody>
         </table>
-        <Pagination itemsCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}/>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </main>
     );
   }
